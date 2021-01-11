00c02f824fd121d6d0ed68e9a3213758ed00cd15
Jerid Fenderson <jrfenderson@gmail.com>
2021-01-11 18:06:18 -0500

    Submitting specifically for PEDAC assignment

--- Program.cs
+++ Program.cs
@@ -5,154 +5,237 @@ namespace AllCardsOnDeck
 {
     class Program
     {
-        static void Main(string[] args)
+
+        class Card
         {
+            public string Suit { get; set; }
+            public string Rank { get; set; }
+            public int RelativeValue()
+            {
 
+                switch (Rank)
+                {
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                    case "6":
+                    case "7":
+                    case "8":
+                    case "9":
+                    case "10":
+                        return int.Parse(Rank);
+                    case "Jack":
+                        return 11;
+                    case "Queen":
+                        return 12;
+                    case "King":
+                        return 13;
+                    case "Ace":
+                        return 1;
+                    default:
+                        return 0;
+                }
+            }
+            public int AbsoluteValue()
+            {
+                var suitValue = 0;
+                var totalValue = 0;
+                switch (Suit)
+                {
+                    case " of Spades":
+                        suitValue = 1;
+                        break;
+                    case " of Clubs":
+                        suitValue = 2;
+                        break;
+                    case " of Hearts":
+                        suitValue = 3;
+                        break;
+                    case " of Diamonds":
+                        suitValue = 4;
+                        break;
+
+                }
+                totalValue = RelativeValue() * suitValue;
+                return totalValue;
+            }
+        }
+        static List<Card> Build(List<Card> deckToBeBuilt)
+        // Method that builds deck, using the Card class to create each card and places each card in a list
+        {
             var listOfSuits = new List<string>()
-            {
-              " of Spades", " of Clubs", " of Hearts", " of Diamonds"
-            };
+                 {
+                    " of Spades", " of Clubs", " of Hearts", " of Diamonds"
+                  };
             var listOfRanks = new List<string>()
-            {
-              "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"
-            };
-            var cardDeckList = new List<string>();
-
+                 {
+                    "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"
+                  };
             foreach (var suit in listOfSuits)
             {
                 foreach (var rank in listOfRanks)
                 {
-                    cardDeckList.Add(rank + suit);
+                    var newCard = new Card()
+                    {
+                        Rank = rank,
+                        Suit = suit,
+                    };
+                    deckToBeBuilt.Add(newCard);
                 }
             }
-
-            foreach (var card in cardDeckList)
-            {
-                Console.WriteLine(card);
-            }
-
-            var cardDeckListValues = new List<int>(){
-            14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-            14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-            14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-            14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,};
-
-
-            var lastUnmovedCardPosition = cardDeckList.Count;
+            return deckToBeBuilt;
+        }
+        static List<Card> Shuffle(List<Card> deckToBeShuffled)
+        // Method that shuffles deck on command
+        {
+            var lastUnmovedCardPosition = deckToBeShuffled.Count;
             var randomNumber = 0;
-            var randomCardBasedOnRandomNumber = "";
-            var randomCardValueBasedOnRandomNumber = 0;
+            var randomCardBasedOnRandomNumber = deckToBeShuffled[randomNumber];
 
             while (lastUnmovedCardPosition-- > 0)
             {
                 randomNumber = new Random().Next(0, lastUnmovedCardPosition);
 
-                randomCardBasedOnRandomNumber = cardDeckList[randomNumber];
-                cardDeckList[randomNumber] = cardDeckList[lastUnmovedCardPosition];
-                cardDeckList[lastUnmovedCardPosition] = randomCardBasedOnRandomNumber;
+                randomCardBasedOnRandomNumber = deckToBeShuffled[randomNumber];
+                deckToBeShuffled[randomNumber] = deckToBeShuffled[lastUnmovedCardPosition];
+                deckToBeShuffled[lastUnmovedCardPosition] = randomCardBasedOnRandomNumber;
+            }
+            return deckToBeShuffled;
+        }
+        static List<Card> Organize(List<Card> deckToBeOrganized)
+        {
+            // - start with the first two cards, in the deck, now referred to as left card and right card
+            var leftCardIndex = 0;
+            var rightCardIndex = 1;
+            var leftCard = deckToBeOrganized[leftCardIndex];
+            var rightCard = deckToBeOrganized[rightCardIndex];
 
-                randomCardValueBasedOnRandomNumber = cardDeckListValues[randomNumber];
-                cardDeckListValues[randomNumber] = cardDeckListValues[lastUnmovedCardPosition];
-                cardDeckListValues[lastUnmovedCardPosition] = randomCardValueBasedOnRandomNumber;
+            // - continue to do the operation below until you reach the last card in the deck and the last card (the right card) is bigger than the second to last card (the left card)
+            do
+            {
+                // - while the right card is bigger than the left card, flip back over the left card
+                while (leftCard.AbsoluteValue() < rightCard.AbsoluteValue())
+                {
+                    // - make the right card the new left card
+                    // - flip over the card to the right of the left card and make that the new right card
+                    if (rightCardIndex < deckToBeOrganized.Count - 1)
+                    {
+                        leftCardIndex++;
+                        rightCardIndex++;
+                    }
+                    leftCard = deckToBeOrganized[leftCardIndex];
+                    rightCard = deckToBeOrganized[rightCardIndex];
+                }
+                // - while the right card is smaller than the left card, swap positions between the left card and the right card
+                while (leftCard.AbsoluteValue() > rightCard.AbsoluteValue())
+                {
+                    // - make the left card the new right card
+                    var transitioningCard = leftCard;
+                    deckToBeOrganized[leftCardIndex] = rightCard;
+                    deckToBeOrganized[rightCardIndex] = transitioningCard;
+                    if (leftCardIndex > 0)
+                    {
+                        leftCardIndex--;
+                        rightCardIndex--;
+                    }
+                    // - flip over the card to the left of the right card and make that the new left card
+                    leftCard = deckToBeOrganized[leftCardIndex];
+                    rightCard = deckToBeOrganized[rightCardIndex];
+                }
+            }
+            while (!((deckToBeOrganized[0].AbsoluteValue() == 1) && (deckToBeOrganized[deckToBeOrganized.Count - 1].AbsoluteValue() == deckToBeOrganized.Count - 1)));
+            return deckToBeOrganized;
+        }
+        static void Main(string[] args)
+        {
+            var deck = new List<Card>();
+            deck = Build(deck);
+
+            Console.WriteLine("\n\nDeck has been built");
+            foreach (var card in deck)
+            {
+                Console.WriteLine(card.Rank + card.Suit);
             }
 
-            Console.WriteLine(cardDeckList[0]);
-            Console.WriteLine(cardDeckList[1]);
+            deck = Shuffle(deck);
+            Console.WriteLine("\n\nDeck has been shuffled");
+            Console.WriteLine($"{deck[0].Rank + deck[0].Suit} and {deck[1].Rank + deck[1].Suit}");
+            Console.WriteLine("...are the first two cards of the shuffled deck\n");
 
             // Adventure mode
-            var playerOnesHand = new List<string>() { };
-            var playerTwosHand = new List<string>() { };
+            var playerOnesHand = new List<Card>();
+            var playerTwosHand = new List<Card>();
 
-            playerOnesHand.Add(cardDeckList[2]);
-            playerOnesHand.Add(cardDeckList[4]);
-            playerTwosHand.Add(cardDeckList[3]);
-            playerTwosHand.Add(cardDeckList[5]);
+            playerOnesHand.Add(deck[2]);
+            playerOnesHand.Add(deck[4]);
+            playerTwosHand.Add(deck[3]);
+            playerTwosHand.Add(deck[5]);
 
-            Console.WriteLine($"Player 1 received a {playerOnesHand[0]} and a {playerOnesHand[1]}");
-            Console.WriteLine($"Player 2 received a {playerTwosHand[0]} and a {playerTwosHand[1]}");
+            Console.WriteLine($"\nPlayer 1 received a {playerOnesHand[0].Rank + playerOnesHand[0].Suit} and a {playerOnesHand[1].Rank + playerOnesHand[1].Suit}");
+            Console.WriteLine($"Player 2 received a {playerTwosHand[0].Rank + playerTwosHand[0].Suit} and a {playerTwosHand[1].Rank + playerTwosHand[1].Suit}\n");
 
             // Epic mode
-            var playerOneWarDeck = new List<string>() { };
-            var playerTwoWarDeck = new List<string>() { };
-            var playerOneWarDeckValues = new List<int>() { };
-            var playerTwoWarDeckValues = new List<int>() { };
+            var playerOneWarDeck = new List<Card>();
+            var playerTwoWarDeck = new List<Card>();
 
             Console.WriteLine("War is beginning soon!");
 
-            for (var i = 0; i < cardDeckList.Count; i++)
+            for (var i = 0; i < deck.Count; i++)
             {
-                playerOneWarDeck.Add(cardDeckList[i]);
-                playerOneWarDeckValues.Add(cardDeckListValues[i]);
+                playerOneWarDeck.Add(deck[i]);
                 i++;
-                playerTwoWarDeck.Add(cardDeckList[i]);
-                playerTwoWarDeckValues.Add(cardDeckListValues[i]);
+                playerTwoWarDeck.Add(deck[i]);
             }
-            var winningDeckSize = playerOneWarDeck.Count;
-            var losingDeckSize = playerTwoWarDeck.Count;
             Console.WriteLine("Decks have been seperated");
             Console.WriteLine("Prepare for battle!");
 
-
-            for (var currentCardInHand = 0; currentCardInHand < losingDeckSize; currentCardInHand++)
+            while (playerOneWarDeck.Count > 1 && playerTwoWarDeck.Count > 1)
             {
-                if ((playerOneWarDeckValues[currentCardInHand] > playerTwoWarDeckValues[currentCardInHand]) && (playerTwoWarDeckValues.Count != 0))
+                if (playerOneWarDeck[0].RelativeValue() > playerTwoWarDeck[0].RelativeValue())
                 {
-                    playerOneWarDeck.Add(playerTwoWarDeck[currentCardInHand]);
-                    playerTwoWarDeck.Remove(playerTwoWarDeck[currentCardInHand]);
-                    playerOneWarDeckValues.Add(playerOneWarDeckValues[currentCardInHand]);
-                    playerTwoWarDeckValues.Remove(playerTwoWarDeckValues[currentCardInHand]);
-                    Console.WriteLine($"Player 2 lost the {playerTwoWarDeck[currentCardInHand]} to the {playerOneWarDeck[currentCardInHand]}");
+                    playerOneWarDeck.Add(playerTwoWarDeck[0]);
+                    playerTwoWarDeck.Remove(playerTwoWarDeck[0]);
+                    Console.WriteLine($"Player 2 lost the {playerTwoWarDeck[0].Rank + playerTwoWarDeck[0].Suit} to the {playerOneWarDeck[0].Rank + playerOneWarDeck[0].Suit}");
                 }
-                else if ((playerOneWarDeckValues[currentCardInHand] < playerTwoWarDeckValues[currentCardInHand]) && (playerOneWarDeckValues.Count != 0))
+                else if (playerOneWarDeck[0].RelativeValue() < playerTwoWarDeck[0].RelativeValue())
                 {
-                    playerTwoWarDeck.Add(playerOneWarDeck[currentCardInHand]);
-                    playerOneWarDeck.Remove(playerOneWarDeck[currentCardInHand]);
-                    playerTwoWarDeckValues.Add(playerTwoWarDeckValues[currentCardInHand]);
-                    playerOneWarDeckValues.Remove(playerOneWarDeckValues[currentCardInHand]);
-                    Console.WriteLine($"Player 1 lost the {playerOneWarDeck[currentCardInHand]} to the {playerTwoWarDeck[currentCardInHand]}");
+                    playerTwoWarDeck.Add(playerOneWarDeck[0]);
+                    playerOneWarDeck.Remove(playerOneWarDeck[0]);
+                    Console.WriteLine($"Player 1 lost the {playerOneWarDeck[0].Rank + playerOneWarDeck[0].Suit} to the {playerTwoWarDeck[0].Rank + playerTwoWarDeck[0].Suit}");
                 }
                 else
                 {
                     Console.WriteLine("War!");
-                    var battleGround = new List<string>() { };
-                    var battleGroundValues = new List<int>() { };
+                    var battleGround = new List<Card>();
                     do
                     {
-                        if ((playerOneWarDeckValues.Count != 0) || (playerTwoWarDeckValues.Count != 0))
-                        {
-                            battleGround.Add(playerOneWarDeck[currentCardInHand]);
-                            battleGround.Add(playerTwoWarDeck[currentCardInHand]);
-                            playerOneWarDeck.Remove(playerOneWarDeck[currentCardInHand]);
-                            playerTwoWarDeck.Remove(playerTwoWarDeck[currentCardInHand]);
-                            battleGroundValues.Add(playerOneWarDeckValues[currentCardInHand]);
-                            battleGroundValues.Add(playerTwoWarDeckValues[currentCardInHand]);
-                            playerOneWarDeckValues.Remove(playerOneWarDeckValues[currentCardInHand]);
-                            playerTwoWarDeckValues.Remove(playerTwoWarDeckValues[currentCardInHand]);
-                            currentCardInHand++;
-                            Console.WriteLine("The battle rages on!");
-                        }
+                        battleGround.Add(playerOneWarDeck[0]);
+                        battleGround.Add(playerTwoWarDeck[0]);
+                        playerOneWarDeck.Remove(playerOneWarDeck[0]);
+                        playerTwoWarDeck.Remove(playerTwoWarDeck[0]);
+                        Console.WriteLine("The battle rages on!");
+
                     }
-                    while (playerOneWarDeckValues[currentCardInHand] == playerTwoWarDeckValues[currentCardInHand]);
+                    while (playerOneWarDeck[0].RelativeValue() == playerTwoWarDeck[0].RelativeValue());
 
                     Console.WriteLine("The victor has risen");
 
                     for (var warLength = battleGround.Count - 1; warLength > 0; warLength--)
                     {
-                        if (playerOneWarDeckValues[currentCardInHand] > playerTwoWarDeckValues[currentCardInHand])
+                        if (playerOneWarDeck[0].RelativeValue() > playerTwoWarDeck[0].RelativeValue())
                         {
                             playerOneWarDeck.Add(battleGround[warLength]);
-                            playerOneWarDeckValues.Add(battleGroundValues[warLength]);
                             Console.WriteLine("Adding cards to the victor's deck");
                         }
                         else
                         {
-                            playerTwoWarDeck.Add(battleGround[warLength]);
-                            playerTwoWarDeckValues.Add(battleGroundValues[warLength]);
+                            playerTwoWarDeck.Add(battleGround[0]);
                             Console.WriteLine("Adding cards to the victor's deck");
                         }
                     }
 
-                    if (playerOneWarDeckValues[currentCardInHand] > playerTwoWarDeckValues[currentCardInHand])
+                    if (playerOneWarDeck[0].RelativeValue() > playerTwoWarDeck[0].RelativeValue())
                     {
                         Console.WriteLine("Player 1 won the battle!");
                     }
@@ -161,26 +244,27 @@ namespace AllCardsOnDeck
                         Console.WriteLine("Player 2 won the battle!");
                     }
                 }
-
-                if (playerOneWarDeckValues.Count >= playerTwoWarDeckValues.Count)
-                {
-                    winningDeckSize = playerOneWarDeckValues.Count;
-                    losingDeckSize = playerTwoWarDeckValues.Count;
-                }
-                else
-                {
-                    winningDeckSize = playerTwoWarDeckValues.Count;
-                    losingDeckSize = playerOneWarDeckValues.Count;
-                }
             }
 
-            if (playerOneWarDeckValues.Count <= 1)
+            if (playerOneWarDeck.Count <= 1)
             {
-                Console.WriteLine("Player Two Won the War!!!");
+                Console.WriteLine("Player 2 Won the War!!!");
+            }
+            else if (playerTwoWarDeck.Count <= 1)
+            {
+                Console.WriteLine("Player 1 Won the War!!!");
             }
             else
             {
-                Console.WriteLine("Player One Won the War!!!");
+                Console.WriteLine("There are no winners in war");
+            }
+
+            Console.WriteLine("Organizing deck...");
+            deck = Organize(deck);
+            Console.WriteLine("\n\nDeck has been organized");
+            foreach (var card in deck)
+            {
+                Console.WriteLine(card.Rank + card.Suit);
             }
 
         }
--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# AllCardsOnDeck
+# AllCardsOnDeck PEDA
+
+## Problem
+
+Create an algorithm to put the cards of a deck in numerical order. The ACE is a one, and the Jack, Queen, King to be in that order after the 10 of the suit.
+
+- you may only turn over TWO cards at any time
+- you may only swap the position of TWO cards (that you have turned over) at any time
+- you may only refer to cards based on their position in your list (left-to-right). You may choose to refer to the position by 0 or by 1, whichever you prefer.
+- you may NOT keep track of the value of any cards in any manner other than by turning over TWO cards and considering their value. For instance, you may not "remember" any values, or write down their values elsewhere, etc
+- take your cards/notes and shuffle them again, laying them out horizontally on the table again
+- invite your friend or family member to follow your algorithm precisely. They may not ask you any clarifying questions. If they do not understand a step you must STOP and revise your algorithm and have them start OVER with a new sorted set of cards/paper
+
+## Examples
+
+List of an Ace, 4, 6, 8, 3, 9, 2 should be made into a list of Ace, 2, 3, 4, 6, 8, 9
+
+## Data
+
+Individual cards
+
+- must contain face
+- must contain suit
+- must be able to calculate value of face
+- will also create value for suit
+
+2 Spots for the cards
+
+- 2 variables that contain the value of 2 cards at any given moment, and compares the value of those two cards and swaps them
+
+## Algorithm
+
+Generate a placeholder individual card
+
+- have the card store the face
+- have the card store the suit
+- have the card determine the face value
+- have the card determine the suit value
+
+Generate a placeholder for a list of cards
+
+- refer to this list of cards as a deck
+
+Generate a list of individual cards
+
+- for each suit in a 52 card deck, assign a card a suit
+- within assigning a card a suit, for each card face in a deck, assign a card a face
+- for each completed card, store the card within a the deck
+
+With the deck now generated, shuffle the deck
+
+- while each card in the deck, starting with the last card in the deck, ending with the first card in the deck, pick up the last card in the deck and place it in a random spot within the deck
+- before fully placing the card within the random spot in the deck, grab the card that the card that the last card in the deck is now replacing and place that card at the end of the deck
+- keep doing this with the next to last card that was moved-s card until you reach the beginning of the deck and the last card that can be moved is the first card
+
+With the deck now shuffled, un-shuffle the deck
+
+- start with the first two cards, in the deck, now referred to as left card and right card
+
+- continue to do the operation below until you reach the last card in the deck and the last card (the right card) is bigger than the second to last card (the left card)
+  {
+- while the right card is bigger than the left card, flip back over the left card
+- make the right card the new left card
+- flip over the card to the right of the left card and make that the new right card
+
+- while the right card is smaller than the left card, swap positions between the left card and the right card
+- make the left card the new right card
+- flip over the card to the left of the right card and make that the new left card
+  }
+
+- the deck is now un-shuffled
